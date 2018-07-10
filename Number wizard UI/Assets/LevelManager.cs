cc9b56a5c96cb1193f17b7c0545f22bf29f72303
Jockum Svanberg <jockum.svanberg@gmail.com>
2018-07-10 08:47:16 +0200

    Stash of old projects and addition of blockbreaker menu system
    I am too lazy to figure this out, should split this up in separate commits and do it proper, also contains a lot of files that should not be there, will look up proper ignore-file in the future

---
+++ Number wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+public void LoadLevel(string name)
+	{
+		Debug.Log("level load requested for: " + name);
+		Application.LoadLevel(name);
+	}
+public void Quit()
+	{
+		Debug.Log("Program exit requested");
+		Application.Quit();
+	}
+}
---
+++ Number wizard UI/Assets/NumberWizard.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	int max = 1000;
+	int min = 1;
+	int guess = 500;
+	public int maxGuessAllowed = 10;
+
+	public Text text;
+
+	void Start ()
+	{
+		StartGame();
+	}
+	void StartGame()
+	{
+		max = 1000;
+		min = 1;
+		guess = 500;
+		maxGuessAllowed = 10;
+	}
+
+	public void GuessHigher()
+	{
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower()
+	{
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess()
+	{
+		guess = (max + min) / 2;
+		text.text = guess.ToString();
+		maxGuessAllowed = maxGuessAllowed - 1;
+		if(maxGuessAllowed <= 0)
+		{
+			Application.LoadLevel("WIN");
+		}
+	}
+}
